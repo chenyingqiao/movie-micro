25cdcc1501d853c1dba56f593e81a23464981362
Lerko <Lerko>
2020-09-05 19:54:35 +0800

    修改域名

---
+++ init-system.sh
@@ -0,0 +1,19 @@
+apt update
+curl -fsSL https://get.docker.com | bash -s docker
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.12.2/minikube-linux-amd64 \
+  && chmod +x minikube
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
+apt-get install conntrack
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectl && mv kubectl /usr/bin
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.6.5 sh -
+echo 'export PATH=/root/istio-1.6.5/bin:$PATH' >> .bashrc
+source .bashrc
+istioctl install --set profile=demo
+
+minikube addons enable ingress
+
+git clone https://github.com/chenyingqiao/movie-micro.git
+
+apt install make

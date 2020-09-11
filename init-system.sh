73345d9ed056cbc3e716e1b9387f626fd40ac633
Lerko <Lerko>
2020-09-11 14:51:23 +0800

    添加提示信息

--- init-system.sh
+++ init-system.sh
@@ -1,21 +1,45 @@
-cd ../$HOME
+echo "工作目录"$HOME
+cd $HOME
 apt update
+
+echo "安装docker"
+
 curl -fsSL https://get.docker.com | bash -s docker
+
+echo "安装minikube"
+
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.12.2/minikube-linux-amd64 \
   && chmod +x minikube
 sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
 apt-get install conntrack
+
+echo "安装kubectl"
+
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x kubectl && mv kubectl /usr/bin
+
+echo "安装启动minikube"
+
+minikube start --driver=none --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus 1
+
+
+echo "安装istio"
+
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.6.5 sh -
 echo 'export PATH=$HOME/istio-1.6.5/bin:$PATH' >> $HOME/.bashrc
 source $HOME/.bashrc
 istioctl install --set profile=demo
 
+echo "开启ingress-controller"
+
 minikube addons enable ingress
 
+echo "克隆项目"
+
 git clone https://github.com/chenyingqiao/movie-micro.git
+
+echo "构建项目"
 
 apt install make
 

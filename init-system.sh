c775792dacfc47d0ea218dc538d88169cb1cb116
Lerko <Lerko>
2020-09-13 21:47:11 +0800

    csh

--- init-system.sh
+++ init-system.sh
@@ -1,4 +1,4 @@
-#!/bin/csh
+#!/bin/sh
 echo "工作目录"$HOME
 cd $HOME
 apt update
@@ -31,10 +31,8 @@ echo "安装istio"
 
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.6.5 sh -
 echo 'export PATH=$HOME/istio-1.6.5/bin:$PATH' >> $HOME/.bashrc
-source $HOME/.bashrc
-export $PATH=$HOME/istio-1.6.5/bin:$PATH
 cd istio-1.6.5/
-istioctl install --set profile=demo
+$HOME/istio-1.6.5/bin/istioctl install --set profile=demo
 cd ..
 
 echo "开启ingress-controller"
